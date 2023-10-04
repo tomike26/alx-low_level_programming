1ed9e4b23e8c5af96ab103e8b2871dcce6dc2505
tomike26 <kolajotomike26@gmail.com>
2023-10-04 16:39:02 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid - a function that creates
+ * a two dimensional array of ints
+ *
+ * @width: width of the matrix
+ * @height: height of the matrix
+ *
+ * Return: pointer to the created matrix (Success) or NULL (Failed)
+ */
