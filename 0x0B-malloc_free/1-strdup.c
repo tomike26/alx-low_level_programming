38195bc4ae8e95a91d87baa845237c2b93d8c384
tomike26 <kolajotomike26@gmail.com>
2023-10-04 16:12:32 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - a function that copies a string given as parameter
+ *
+ * @str: string to duplicate
+ *
+ * Return: a pointer to the copied string (Success), NULL (Falled)
+ */
+char *_strdup(char *str)
+{
