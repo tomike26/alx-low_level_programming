1b5d0465dd254b10a24c65d58b2c9ce88a5d98d9
tomike26 <kolajotomike26@gmail.com>
2023-10-04 16:28:40 -0700

    2-main.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *str_concat - c function that catenates two strings
+ *
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
+ *
+ * Return: pointer to the new string created (Success), or NULL (Failed)
+ */
+char *str_concat(char *s1, char *s2)
